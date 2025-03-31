3b44ede6e6c639661b18f23f66e76e73831ea8c5
Preston Rogers <parogers@alumni.stanford.edu>
2025-03-30 19:24:25 -0700

    Adding the niseko boarding pic

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,3 +1,26 @@
-import React from "react";
-const AboutMe = () => <div><h1>About Me</h1><p>This is the About Me page.</p></div>;
+import React from 'react';
+import '../styles/AboutMe.css'; // Make sure to import your CSS file
+
+const AboutMe = () => {
+  return (
+    <div className="about-me">
+      {/* Text overlay */}
+      <div className="about-me-text">
+        About Me
+      </div>
+
+      {/* Content below the image */}
+      <div className="about-me-content">
+        <p>
+          Here is some content about yourself. As you scroll down, the text will scroll up, and the background image will scroll at a slower rate.
+        </p>
+        {/* Add more content as needed */}
+      </div>
+
+      {/* The red bar */}
+      <div className="red-bar"></div>
+    </div>
+  );
+};
+
 export default AboutMe;
